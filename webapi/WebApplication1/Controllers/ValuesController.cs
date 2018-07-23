a707de6153b8a0646d1d49c2bdc1c407ed36aeac
unknown <artemdude@gmail.com>
2018-07-23 13:08:55 +0100

    fff

---
+++ webapi/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public string Get(string id)
+        {
+            MetroManager.MetroLauncher.LaunchApp("Microsoft.MSPaint_5.1806.20057.0_x64__8wekyb3d8bbwe");
+
+
+			//RunProcess(@"C:\Users\arshkolo\AppData\Roaming\Spotify\Spotify.exe");
+            return "OK";
+        }
+
+	    private void RunProcess(string path)
+	    {
+		    var p = new Process
+		    {
+			    StartInfo =
+			    {
+				    FileName = path,
+				    CreateNoWindow = true
+			    }
+		    };
+		    p.Start();
+        }
+    }
+}
